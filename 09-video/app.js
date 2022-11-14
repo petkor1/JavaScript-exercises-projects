e0004cd264a5294c2d21b027c46100b2130319b0
Piotr Korbas <piotrkorbas@gmail.com>
2022-11-14 19:23:14 +0100

    09- video

--- 09-video/app.js
+++ 09-video/app.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector('.switch-btn');
 const video = document.querySelector('.video-container');
 
+
 btn.addEventListener('click', function () {
     if (!btn.classList.contains('slide')) {
         btn.classList.add('slide');
@@ -10,3 +11,10 @@ btn.addEventListener('click', function () {
         video.play();
     }
 })
+
+// preloader
+const preloader = document.querySelector('.preloader');
+window.addEventListener('load', function () {
+    preloader.classList.add('hide-preloader')
+})
+
