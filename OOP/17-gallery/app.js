7b5f71a8d73e56cb686c009fb97ab5f4dc2311ac
Piotr Korbas <piotrkorbas@gmail.com>
2022-11-22 13:59:46 +0100

    17 galery, slider

--- OOP/17-gallery/app.js
+++ OOP/17-gallery/app.js
@@ -8,39 +8,90 @@ function getElement(selection) {
   );
 }
 
-function Gallery(element) {
-  this.container = element
-  this.list = [...element.querySelectorAll('.img')];
-  // target
-  this.modal = getElement('.modal');
-  this.modalImg = getElement('.main-img');
-  this.imageName = getElement('.image-name');
-  this.modal = getElement('.modal');
-  this.modalImages = getElement('.modal-images');
-  this.closeBtn = getElement('.close-btn')
-  this.nextBtn = getElement('.next-btn')
-  this.prevBtn = getElement('.prev-btn')
-  // bind function
-  // this.openModal = this.openModal.bind(this);
-  this.container.addEventListener('click', function (e) {
-    if (e.target.classList.contains('img')) {
-      this.openModal(e.target, this.list);
+class Gallery {
+  constructor(element) {
+    this.container = element;
+    this.list = [...element.querySelectorAll('.img')];
+    // target
+    this.modal = getElement('.modal');
+    this.modalImg = getElement('.main-img');
+    this.imageName = getElement('.image-name');
+    this.modalImages = getElement('.modal-images');
+    this.closeBtn = getElement('.close-btn');
+    this.nextBtn = getElement('.next-btn');
+    this.prevBtn = getElement('.prev-btn');
+    // self ref
+    // let self = this;
+    // bind functions
+    // this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.nextImage = this.nextImage.bind(this);
+    this.prevImage = this.prevImage.bind(this);
+    this.chooseImage = this.chooseImage.bind(this);
+    // container event
+    this.container.addEventListener(
+      'click',
+      function (e) {
+        // self.openModal();
+        if (e.target.classList.contains('img')) {
+          this.openModal(e.target, this.list);
+        }
+      }.bind(this)
+    );
+  }
+  openModal(selectedImage, list) {
+    this.setMainImage(selectedImage);
+    this.modalImages.innerHTML = list
+      .map(function (image) {
+        return `<img src="${image.src
+          }" title="${image.title}" data-id="${image.dataset.id}" class="${selectedImage.dataset.id === image.dataset.id ? 'modal-img selected' : 'modal-img'}"/>`;
+      })
+      .join('');
+    this.modal.classList.add('open');
+    this.closeBtn.addEventListener('click', this.closeModal);
+    this.nextBtn.addEventListener('click', this.nextImage);
+    this.prevBtn.addEventListener('click', this.prevImage);
+    this.modalImages.addEventListener('click', this.chooseImage);
+  }
+
+  setMainImage(selectedImage) {
+    this.modalImg.src = selectedImage.src;
+    this.imageName.textContent = selectedImage.title;
+  }
+
+  closeModal() {
+    this.modal.classList.remove('open');
+    this.closeBtn.removeEventListener('click', this.closeModal);
+    this.nextBtn.removeEventListener('click', this.nextImage);
+    this.prevBtn.removeEventListener('click', this.prevImage);
+    this.modalImages.removeEventListener('click', this.chooseImage);
+  }
+  nextImage() {
+    const selected = this.modalImages.querySelector('.selected');
+    const next =
+      selected.nextElementSibling || this.modalImages.firstElementChild;
+    selected.classList.remove('selected');
+    next.classList.add('selected');
+    this.setMainImage(next);
+  }
+  prevImage() {
+    const selected = this.modalImages.querySelector('.selected');
+    const prev =
+      selected.previousElementSibling || this.modalImages.lastElementChild;
+    selected.classList.remove('selected');
+    prev.classList.add('selected');
+    this.setMainImage(prev);
+  }
+  chooseImage(e) {
+    if (e.target.classList.contains('modal-img')) {
+      const selected = this.modalImages.querySelector('.selected');
+      selected.classList.remove('selected');
+
+      this.setMainImage(e.target);
+      e.target.classList.add('selected');
     }
-
-  }.bind(this));
+  }
 }
 
-Gallery.prototype.openModal = function (selectedImage, list) {
-  this.setMainImage(selectedImage);
-  this.modalImages.innerHTML = list.map(function (image) {
-    return `<img src="${image.src}" title="${image.title}" alt="${image.title}" class="${selectedImage.dataset.id === image.dataset.id ? "modal-img selected" : "modal-img"}" data-id="${image.dataset.id}">`
-  }).join('');
-  this.modal.classList.add('open')
-}
-Gallery.prototype.setMainImage = function (selectedImage) {
-  this.modalImg.src = selectedImage.src;
-  this.imageName.textContent = selectedImage.title;
-}
-
-const nature = new Gallery(getElement('.nature'))
-const city = new Gallery(getElement('.city'))
+const nature = new Gallery(getElement('.nature'));
+const city = new Gallery(getElement('.city'));
