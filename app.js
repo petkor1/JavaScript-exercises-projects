a631951f725dbe7504d2ed446ea21fb47e265ec6
Piotr Korbas <piotrkorbas@gmail.com>
2022-12-01 15:10:43 +0100

    ajax, promise, async await

--- app.js
+++ app.js
@@ -1,8 +1,88 @@
-const product = {
-    title: 'condom exclusive',
-    price: '90',
-    size: 'large'
-}
+// const xhr = new XMLHttpRequest();
+// const btn = document.querySelector('.btn');
+// const url = './api/people.json'
 
-const result = product.title.includes();
-console.log();
+// btn.addEventListener('click', () => {
+//     getData(url);
+// })
+
+// function getData(url) {
+//     xhr.open('GET', url);
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState == 4 && xhr.status === 200) {
+//             const data = JSON.parse(xhr.responseText);
+//             const displayData = data.map((item) => {
+//                 return `<p>name: ${item.name}</p>`
+//             }).join('')
+//             const element = document.createElement('div');
+//             element.innerHTML = displayData;
+//             document.body.appendChild(element);
+//         } else {
+//             console.log({
+//                 status: xhr.status,
+//                 text: xhr.statusText
+//             });
+//         }
+//     }
+//     xhr.send();
+// }
+const url = './api/people.json';
+// const response = fetch(url);
+// console.log(response);
+
+// fetch(url).then((resp) => {
+//     // response obcject
+//     // useful prop and methods
+//     // console.log(resp);
+//     // convert response into JSON data
+//     // return promise
+//     return resp.json()
+// }).then((data) => {
+//     console.log(data);
+// }).catch((err) => console.log(err));
+
+// fetch(url)
+//     .then(resp => resp.json())
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('click', () => {
+//     fetch(url)
+//         .then(resp => resp.json())
+//         .then(data => displayItems(data))
+//         .catch(err => console.log(err));
+// })
+
+// const displayItems = (items) => {
+//     const displayData = items.map((item) => {
+//         const { name } = item
+//         return `<p>${name}</p>`
+//     }).join('')
+//     const element = document.createElement('div');
+//     element.innerHTML = displayData;
+//     document.body.appendChild(element);
+// }
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', async () => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        displayItems(data);
+    } catch (error) {
+        console.log(
+            error
+        );
+    }
+})
+
+const displayItems = (items) => {
+    const displayData = items.map((item) => {
+        const { name } = item
+        return `<p>${name}</p>`
+    }).join('')
+    const element = document.createElement('div');
+    element.innerHTML = displayData;
+    document.body.appendChild(element);
+}
---
+++ utils/data.js
@@ -0,0 +1,7 @@
+export const random = 'random value';
+
+export const people = [
+    { name: 'john', job: 'developer' },
+    { name: 'susan', job: 'the boss' },
+    { name: 'anna', job: 'designer' }
+]
