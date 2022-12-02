71408b46a990470567e695f7cba8ec81ddcf070b
Piotr Korbas <piotrkorbas@gmail.com>
2022-12-02 15:07:52 +0100

    22-products

---
+++ 22-products/app.js
@@ -0,0 +1,41 @@
+const url = 'https://course-api.com/javascript-store-products'
+
+const productsDOM = document.querySelector('.products-center');
+
+const fetchProducts = async () => {
+    productsDOM.innerHTML = `<div class="loading"></div>`
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('there was an error');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        productsDOM.innerHTML = `<p class="error">there was an error</p>`
+    }
+}
+
+const displayProducts = (list) => {
+    const productList = list.map(product => {
+        const { id } = product;
+        const { name: title, price } = product.fields;
+        const { url: img } = product.fields.image[0]
+        const formatPrice = price / 100;
+        return `<a id="${id}" href="product.html?id=${id}" class="single-product">
+        <img src="${img}" alt="${title}" class="single-product-img img">
+        <footer>
+            <h5 class="name">${title}</h5>
+            <span class="price">$${formatPrice}</span>
+        </footer>
+    </a>`
+    }).join('');
+    productsDOM.innerHTML = `<div class="products-container">${productList}</div>`
+}
+
+const start = async () => {
+    const data = await fetchProducts();
+    displayProducts(data);
+}
+
+start();
