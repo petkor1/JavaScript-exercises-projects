26636818120d64d6373beb41b4839aef4c7f9fcb
Piotr Korbas <piotrkorbas@gmail.com>
2022-12-02 17:18:46 +0100

    23 random user

---
+++ 23-random-user/app.js
@@ -0,0 +1,9 @@
+import getEl from "./utils/getElement.js";
+const URL = 'https://randomuser.me/api/';
+
+const userImg = getEl('.user-img')
+const title = getEl('.user-title')
+const value = getEl('.user-value')
+const btn = getEl('.btn')
+const btns = [...document.querySelectorAll('.icon')]
+
