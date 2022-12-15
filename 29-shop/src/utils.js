7e6aa34320f8786958c3417901ee4f1dc69e1aae
Piotr Korbas <piotrkorbas@gmail.com>
2022-12-15 12:47:28 +0100

    29 shop

--- 29-shop/src/utils.js
+++ 29-shop/src/utils.js
@@ -1,8 +1,3 @@
-//   ATTENTION!!!!!!!!!!!
-//   I SWITCHED TO PERMANENT DOMAIN
-//   DATA IS THE SAME JUST A DIFFERENT URL,
-//   DOES NOT AFFECT PROJECT FUNCTIONALITY
-
 const allProductsUrl = 'https://course-api.com/javascript-store-products'
 // temporary single product
 // 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
@@ -15,10 +10,26 @@ const getElement = (selection) => {
   throw new Error(`Please check "${selection}" selector, no such element exist`);
 }
 
-const formatPrice = () => { }
+const formatPrice = (price) => {
+  let formattedPrice = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format((price / 100).toFixed(2));
+  return formattedPrice
+}
 
-const getStorageItem = () => { }
-const setStorageItem = () => { }
+const getStorageItem = (item) => {
+  let storageItem = localStorage.getItem(item);
+  if (storageItem) {
+    storageItem = JSON.parse(localStorage.getItem(item))
+  } else {
+    storageItem = [];
+  }
+  return storageItem;
+}
+const setStorageItem = (name, item) => {
+  localStorage.setItem(name, JSON.stringify(item))
+}
 
 export {
   allProductsUrl,
