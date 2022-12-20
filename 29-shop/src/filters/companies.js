c969c49d7c625a1dbb748e1c5fe6b43364e2616b
Piotr Korbas <piotrkorbas@gmail.com>
2022-12-20 10:09:45 +0100

    29 shop

--- 29-shop/src/filters/companies.js
+++ 29-shop/src/filters/companies.js
@@ -1,6 +1,27 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
-const setupCompanies = () => {};
+const setupCompanies = (store) => {
+    let companies = ['all', ...new Set(store.map(product => product.company))];
+    const companiesDOM = getElement('.companies');
+    companiesDOM.innerHTML = companies.map(company => {
+        return `<button class="company-btn">${company}</button>`
+    }).join('');
+    companiesDOM.addEventListener('click', function (e) {
+        const element = e.target;
+        if (element.classList.contains('company-btn')) {
+            const text = element.textContent
+            let newStore = [];
+            if (text === 'all') {
+                newStore = [...store];
+            } else {
+                newStore = store.filter(product => product.company == text)
+            }
+            display(newStore, getElement('.products-container'))
+
+        }
+
+    })
+};
 
 export default setupCompanies;
